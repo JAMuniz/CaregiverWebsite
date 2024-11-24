c859d8280b0c061c861663ec3517f26888967a8d
Jose Neto <169936540+JAMuniz@users.noreply.github.com>
2024-11-24 13:21:03 -0500

    Made Sign up and Lofin text links.

--- client/src/Pages/Home.js
+++ client/src/Pages/Home.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import '../css/Home.css';
 import Navbar from '../Components/Navbar';
 
@@ -13,7 +14,10 @@ function Home() {
                     Connecting families with trusted caregivers for their loved ones.
                 </p>
                 <p>
-                    Sign up or log in to find a caregiver, manage your profile, or track your service contracts.
+                    <Link to="/signin" className="Home-link">Sign up</Link>
+                    {' '}or {' '}
+                    <Link to="/login" className="Home-link">Log In</Link> 
+                    {' '} to find a caregiver, manage your profile, or track your service contracts.
                 </p>
             </header>
         </div>
