60958cf24aa213f53dc648041cc27f0a182791e5
RemyMB <brandonbedard12@gmail.com>
2024-11-27 15:32:50 -0500

    fixed a gbj in /home
    gbj = gay baby jail = softlock = the user is stuck or forced to do something stupid

--- client/src/Pages/Home.js
+++ client/src/Pages/Home.js
@@ -4,21 +4,35 @@ import '../css/Home.css';
 import Navbar from '../Components/Navbar';
 
 function Home() {
+  const userName = localStorage.getItem('name');
+
   return (
     <div>
         < Navbar />
         <div className="Home">
             <header className="Home-header">
-                <h1>Welcome to the Caregiver Website</h1>
-                <p>
-                    Connecting families with trusted caregivers for their loved ones.
-                </p>
-                <p>
-                    <Link to="/signin" className="Home-link">Sign Up</Link>
-                    {' '}or {' '}
-                    <Link to="/login" className="Home-link">Log In</Link> 
-                    {' '} to find a caregiver, manage your profile, or track your service contracts.
-                </p>
+                {userName ? (
+                    <>
+                    <h1>Welcome back!</h1>
+                    <p>
+                        <Link to="/welcome" className="Home-link">Click here</Link>
+                        {' '} to enter your dashboard.
+                    </p>
+                    </>
+                ) : (
+                    <>
+                    <h1>Welcome to the Caregiver Website</h1>
+                    <p>
+                        Connecting families with trusted caregivers for their loved ones.
+                    </p>
+                    <p>
+                        <Link to="/signin" className="Home-link">Sign Up</Link>
+                        {' '}or {' '}
+                        <Link to="/login" className="Home-link">Log In</Link> 
+                        {' '} to find a caregiver, manage your profile, or track your service contracts.
+                    </p>
+                    </>
+                )}
             </header>
         </div>
     </div>
