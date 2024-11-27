d322815857d74cf224be612a95ce7fd3f9a1521a
RemyMB <brandonbedard12@gmail.com>
2024-11-27 17:31:00 -0500

    welcome routing endpoint separation

--- client/src/Pages/Welcome.js
+++ client/src/Pages/Welcome.js
@@ -1,9 +1,20 @@
-import React from 'react';
+import React, { useState } from 'react';
 import '../css/welcome.css';
 import Navbar from '../Components/Navbar';
 
 function Welcome() {
 
+  const [route, setRoute] = useState('');
+
+  let message;
+  if (route === "search") {
+    message = <p>search</p>
+  } else if (route === "account") {
+    message = <p>account</p>
+  } else {
+    message = <p>This is your personalized dashboard. Use the menu on the left to navigate.</p>;
+  };
+  
   return (
     <div>
         < Navbar />
@@ -11,13 +22,13 @@ function Welcome() {
         <aside className="Sidebar">
             <h2>Menu</h2>
             <ul>
-                <li><a href="#search">Search Service</a></li>
-                <li><a href="#account">Account</a></li>
+                <li><a href="#search" onClick = {() => setRoute("search")}>Search Service</a></li>
+                <li><a href="#account" onClick = {() => setRoute("account")}>Account</a></li>
             </ul>
         </aside>
 
         <main className="Main-content">
-            <p>This is your personalized dashboard. Use the menu on the left to navigate.</p>
+            {message}
         </main>
         </div>
     </div>
